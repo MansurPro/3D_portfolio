ee4d1cb038b702a6ede466651c010c06e904e3e8
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-03-15 17:22:00 -0500

    Css settings, Navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,8 +26,17 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p ></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Mansurbek <span className='sm:block hidden'>| Satarov</span>
+          </p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
